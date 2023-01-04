c2a54eefb09571d91605ec2ba7de15ab060b9720
Saavedra98 <saavedraerazoc@gmail.com>
2023-01-04 14:13:55 -0500

    fin de clase

--- src/app/header/home/home.component.ts
+++ src/app/header/home/home.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.formLogin = this.formBuilder.group({
+      nombre: ['',
+        [
+          Validators.required,
+          Validators.minLength(10)
+        ]
+      ],
+
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      mensaje: ['',
+        [
+          Validators.required,
+          Validators.minLength(5)
+        ]
+      ]
+
+    })
+
+  }
+
+  send(): any {
+    console.log(this.formLogin.value)
+  }
 
 }
